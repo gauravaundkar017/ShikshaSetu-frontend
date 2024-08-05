676a93d35647a360918415ab777d1a3c299d1c56
gsaundkar <aundkargaurav@gamil.com>
2024-08-06 01:17:11 +0530

    Completed course list page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom'
 
 import AboutUs from './Pages/AboutUs';
+import CourseList from './Pages/Course/CourseList';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/about' element={<AboutUs/>}></Route>
         <Route path='/signup' element={<SignUp/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
+        <Route path='/courses' element={<CourseList/>}></Route>
 
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
--- src/Redux/Slices/CourseSlice.js
+++ src/Redux/Slices/CourseSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 import axiosInstance from "../../Helpers/axiosInstance";
 
@@ -11,22 +11,29 @@ export const getAllCourses = createAsyncThunk("/course/get", async () => {
     try {
         const response = axiosInstance.get("/courses");
         toast.promise(response, {
-            loading : "Loading course data...",
-            success : "Course loaded successfully",
-            error : "Failed to get the courses"
+            loading: "loading course data...",
+            success: "Courses loaded successfully",
+            error: "Failed to get the courses",
         });
+
         return (await response).data.courses;
-    } catch (error) {
+    } catch(error) {
         toast.error(error?.response?.data?.message);
     }
-})
+}); 
 
 const courseSlice = createSlice({
-    name : "courses",
+    name: "courses",
     initialState,
-    reducers : {},
-    extraReducers : (builder) => {
-
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(getAllCourses.fulfilled, (state, action) => {
+            if(action.payload) {
+                console.log(action.payload);
+                
+                state.courseData = [...action.payload];
+            }
+        })
     }
 });
 
