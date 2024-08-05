9417ba67c17d7bcbda8a7138ef1624234a7d888b
gsaundkar <aundkargaurav@gamil.com>
2024-08-05 22:26:50 +0530

    Added sign in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import AboutUs from './Pages/AboutUs';
 import HomePage from './Pages/HomePage';
+import Login from './Pages/Login';
 import NotFound from './Pages/NotFound';
 import SignUp from './Pages/SignUp';
 
@@ -16,6 +17,8 @@ function App() {
         <Route path='/' element={<HomePage/>}></Route>
         <Route path='/about' element={<AboutUs/>}></Route>
         <Route path='/signup' element={<SignUp/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
      
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -19,8 +19,23 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) =>{
             },
             error: "Failed to create account"
         });
+        return (await res).data;
         
-        
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+});
+
+export const login = createAsyncThunk("/auth/login", async (data) =>{
+    try {
+        const res = axiosInstance.post("/user/login", data);
+        toast.promise( res,{
+            loading: "Wait! authentication in progress...",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to log in"
+        });
         return (await res).data;
         
     } catch (error) {
@@ -31,7 +46,18 @@ export const createAccount = createAsyncThunk("/auth/signup", async (data) =>{
 const authSlice = createSlice({
     name : 'auth',
     initialState,
-    reducers: {},
+    reducers : {},
+    extraReducers : (builder) =>{
+        builder
+        .addCase(login.fulfilled, (state, action) => {
+            localStorage.setItem("data", JSON.stringify(action?.payload?.user));
+            localStorage.setItem("isLoggedIn", true);
+            localStorage.setItem("role", action?.payload?.user?.role);
+            state.isLoggedIn = true;
+            state.data = action?.payload?.user;
+            state.role = action?.payload?.user?.role
+        })
+    }
 });
 
 // export const {} = authSlice.actions;
